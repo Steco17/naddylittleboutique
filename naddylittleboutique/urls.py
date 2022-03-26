f482a56bc1e3010548a743255d2de7af6e38f6e3
Steco17 <moyo.stephane@gmail.com>
2022-03-26 03:15:08 +0000

    feat:setting static files

--- naddylittleboutique/urls.py
+++ naddylittleboutique/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('register/',views.register_page, name='register'),
     path('about/',views.about_page, name='about'),
     path('contact/',views.contact_page, name='contact'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
